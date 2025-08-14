0d49dcee531dbbee8c66d8e49263d3894aa935d5
Reeman Singh <reemanl.singh422@gmail.com>
2025-08-14 21:45:06 +0800

    feat: add info bar with coordinates and zoom display

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,16 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+
+const INITIAL_COORDS: [number, number] = [-157.3637, 1.9827]; // banan
+const INITIAL_ZOOM = 13;
 
 function Map() {
   const mapRef = useRef<mapboxgl.Map>(null);
   const mapContainerRef = useRef<HTMLDivElement>(null);
+
+  const [center, setCenter] = useState(INITIAL_COORDS);
+  const [zoom, setZoom] = useState(INITIAL_ZOOM);
 
   useEffect(() => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
@@ -13,8 +19,20 @@ function Map() {
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
         style: "mapbox://styles/mapbox/standard-satellite",
-        center: [-74.006, 40.7128],
-        zoom: 10,
+        center: center,
+        zoom: zoom,
+      });
+    }
+
+    if (mapRef.current) {
+      mapRef.current.on("move", () => {
+        // get the current center coordinates and zoom level from the map
+        const mapCenter = mapRef.current!.getCenter();
+        const mapZoom = mapRef.current!.getZoom();
+
+        // update state
+        setCenter([mapCenter.lng, mapCenter.lat]);
+        setZoom(mapZoom);
       });
     }
 
@@ -27,6 +45,10 @@ function Map() {
 
   return (
     <>
+      <div id="coords">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
+        Zoom: {zoom.toFixed(2)}
+      </div>
       <div
         id="map-container"
         ref={mapContainerRef}
